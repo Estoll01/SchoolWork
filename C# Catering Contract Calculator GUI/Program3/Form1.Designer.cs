80ca5c8dd051334749123ad3fde08b8a635f17fa
Ethan <ethan.tolliver0005@gmail.com>
2025-11-24 21:44:17 -0500

    Add files via upload

---
+++ C# Catering Contract Calculator GUI/Program3/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Program3
+{
+    partial class ContractCal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CaterLb = new System.Windows.Forms.Label();
+            this.BusinessLb = new System.Windows.Forms.Label();
+            this.ContractYearLb = new System.Windows.Forms.Label();
+            this.ContractPriceLb = new System.Windows.Forms.Label();
+            this.TitleLb = new System.Windows.Forms.Label();
+            this.Calbutton = new System.Windows.Forms.Button();
+            this.BusinessCB = new System.Windows.Forms.ComboBox();
+            this.CatererCB = new System.Windows.Forms.ComboBox();
+            this.ContractPriceTotal = new System.Windows.Forms.Label();
+            this.ContractYearTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CaterLb
+            // 
+            this.CaterLb.AutoSize = true;
+            this.CaterLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CaterLb.Location = new System.Drawing.Point(51, 72);
+            this.CaterLb.Name = "CaterLb";
+            this.CaterLb.Size = new System.Drawing.Size(66, 20);
+            this.CaterLb.TabIndex = 0;
+            this.CaterLb.Text = "Caterer:";
+            // 
+            // BusinessLb
+            // 
+            this.BusinessLb.AutoSize = true;
+            this.BusinessLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BusinessLb.Location = new System.Drawing.Point(51, 127);
+            this.BusinessLb.Name = "BusinessLb";
+            this.BusinessLb.Size = new System.Drawing.Size(78, 20);
+            this.BusinessLb.TabIndex = 1;
+            this.BusinessLb.Text = "Business:";
+            // 
+            // ContractYearLb
+            // 
+            this.ContractYearLb.AutoSize = true;
+            this.ContractYearLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContractYearLb.Location = new System.Drawing.Point(51, 191);
+            this.ContractYearLb.Name = "ContractYearLb";
+            this.ContractYearLb.Size = new System.Drawing.Size(120, 20);
+            this.ContractYearLb.TabIndex = 2;
+            this.ContractYearLb.Text = "Contract Years:";
+            // 
+            // ContractPriceLb
+            // 
+            this.ContractPriceLb.AutoSize = true;
+            this.ContractPriceLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContractPriceLb.Location = new System.Drawing.Point(51, 320);
+            this.ContractPriceLb.Name = "ContractPriceLb";
+            this.ContractPriceLb.Size = new System.Drawing.Size(113, 20);
+            this.ContractPriceLb.TabIndex = 3;
+            this.ContractPriceLb.Text = "Contract Price:";
+            // 
+            // TitleLb
+            // 
+            this.TitleLb.AutoSize = true;
+            this.TitleLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLb.Location = new System.Drawing.Point(155, 9);
+            this.TitleLb.Name = "TitleLb";
+            this.TitleLb.Size = new System.Drawing.Size(311, 25);
+            this.TitleLb.TabIndex = 4;
+            this.TitleLb.Text = "Catering Contract Calculator";
+            // 
+            // Calbutton
+            // 
+            this.Calbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calbutton.Location = new System.Drawing.Point(160, 251);
+            this.Calbutton.Name = "Calbutton";
+            this.Calbutton.Size = new System.Drawing.Size(146, 23);
+            this.Calbutton.TabIndex = 5;
+            this.Calbutton.Text = "Calculate";
+            this.Calbutton.UseVisualStyleBackColor = true;
+            this.Calbutton.Click += new System.EventHandler(this.Calbutton_Click);
+            // 
+            // BusinessCB
+            // 
+            this.BusinessCB.FormattingEnabled = true;
+            this.BusinessCB.Items.AddRange(new object[] {
+            "Humana",
+            "KFC",
+            "LGE",
+            "GE",
+            "Alpine Intel"});
+            this.BusinessCB.Location = new System.Drawing.Point(345, 126);
+            this.BusinessCB.Name = "BusinessCB";
+            this.BusinessCB.Size = new System.Drawing.Size(121, 21);
+            this.BusinessCB.TabIndex = 6;
+            // 
+            // CatererCB
+            // 
+            this.CatererCB.FormattingEnabled = true;
+            this.CatererCB.Items.AddRange(new object[] {
+            "Shalimar",
+            "Bombay Grill",
+            "Clay Oven",
+            "Little India",
+            "India Palace"});
+            this.CatererCB.Location = new System.Drawing.Point(345, 72);
+            this.CatererCB.Name = "CatererCB";
+            this.CatererCB.Size = new System.Drawing.Size(121, 21);
+            this.CatererCB.TabIndex = 7;
+            // 
+            // ContractPriceTotal
+            // 
+            this.ContractPriceTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ContractPriceTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContractPriceTotal.Location = new System.Drawing.Point(341, 320);
+            this.ContractPriceTotal.Name = "ContractPriceTotal";
+            this.ContractPriceTotal.Size = new System.Drawing.Size(120, 20);
+            this.ContractPriceTotal.TabIndex = 8;
+            // 
+            // ContractYearTB
+            // 
+            this.ContractYearTB.Location = new System.Drawing.Point(341, 193);
+            this.ContractYearTB.Name = "ContractYearTB";
+            this.ContractYearTB.Size = new System.Drawing.Size(120, 20);
+            this.ContractYearTB.TabIndex = 9;
+            // 
+            // ContractCal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 450);
+            this.Controls.Add(this.ContractYearTB);
+            this.Controls.Add(this.ContractPriceTotal);
+            this.Controls.Add(this.CatererCB);
+            this.Controls.Add(this.BusinessCB);
+            this.Controls.Add(this.Calbutton);
+            this.Controls.Add(this.TitleLb);
+            this.Controls.Add(this.ContractPriceLb);
+            this.Controls.Add(this.ContractYearLb);
+            this.Controls.Add(this.BusinessLb);
+            this.Controls.Add(this.CaterLb);
+            this.Name = "ContractCal";
+            this.Text = "Contract Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CaterLb;
+        private System.Windows.Forms.Label BusinessLb;
+        private System.Windows.Forms.Label ContractYearLb;
+        private System.Windows.Forms.Label ContractPriceLb;
+        private System.Windows.Forms.Label TitleLb;
+        private System.Windows.Forms.Button Calbutton;
+        private System.Windows.Forms.ComboBox BusinessCB;
+        private System.Windows.Forms.ComboBox CatererCB;
+        private System.Windows.Forms.Label ContractPriceTotal;
+        private System.Windows.Forms.TextBox ContractYearTB;
+    }
+}
+
---
+++ C# Catering Contract Calculator GUI/Program3/Form1.cs
@@ -0,0 +1,131 @@
+﻿//Ethan Tolliver
+//Program 3
+//CIS-199-50-4258
+//Due Date: 11/7/2025
+//This program calculates the total contract price for catering services based on user selections and inputs.
+/*1.What is an “off by one error” in relation to arrays in C#?
+    An "off by one error" occurs when a array element is outside of the valid range. For example, trying to access -1 when the minimum index is 0.
+2.Why do we make arrays?
+    We make arrays to store multiple values of the same type in a single variable, allowing for efficient data management.
+3.What is an initializer list?
+    An initializer list is a way to declare and initialize an array in a single statement, using {} to specify the elements. For example int[] numbers = {1, 2, 3, 4, 5};
+4.When would we use a foreach loop?
+    You use a foreach loop when you dont need to modify the elements of a collection and just want to iterate through them for reading or processing. It is used for simplicity
+    and is also usually read-only.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program3
+{
+    public partial class ContractCal : Form
+    {
+        public ContractCal()
+        {
+            InitializeComponent();
+        }
+
+        private void Calbutton_Click(object sender, EventArgs e)
+        {
+
+            //Array--------------------------------------//
+            //Caterer Names and Discounts
+            string[] catererName = { "Shalimar", "Bombay Grill", "Clay Oven", "Little India", "India Palace" };//Caterer Names
+            double[] catererDiscount = { 0.2, 0.17, 0.15, 0.10, 0.08 };//Corresponding Discounts
+
+            //Business Names and Base Prices
+            string[] businessName = { "Humana", "KFC", "LGE", "GE", "Alpine Intel" };//Business Names
+            double[] businessContractPrice = { 1000, 850, 700, 650, 500 };//Corresponding Base Prices
+
+            //Years & additional discount
+            int [] contractYears = { 0, 3, 7, 11 };//Contract Years
+            double[] additionalDiscount = { 50, 100, 150, 200 };//Corresponding Additional Discounts
+
+            //Variables
+            double basePrice = 0;//Business Base Price
+            double catererDisc = 0;//Caterer Discount
+            int minYears = 0;//Contract Years
+            double additionalDisc = 0;//Additional Discount
+
+
+
+            //Validation Checks----------------------------------//
+            if (!(CatererCB.SelectedIndex >=0 ))//No selection made
+            {
+                MessageBox.Show("Please select a Caterer.");
+                return;
+            }
+
+            //Business Validation
+            if (!(BusinessCB.SelectedIndex >= 0))//No selection made
+            {
+                MessageBox.Show("Please select a Business.");
+                return;
+            }
+
+            //Contract Years Validation
+            if (!int.TryParse(ContractYearTB.Text, out minYears) || minYears < 0)//Invalid input/ less than 0
+            {
+                MessageBox.Show("Please enter a valid number of contract years (0 or greater).");
+                return;
+            }
+
+
+
+
+            //Get Values from Arrays--------------------------------//
+            for (int i = 0; i < catererName.Length; i++)//Get Caterer Discount
+            {
+                if (CatererCB.SelectedItem.ToString() == catererName[i])
+                {
+                    catererDisc = catererDiscount[i];
+                    break;
+                }
+            }
+
+            //Get Business Base Price
+            for (int i = 0; i < businessName.Length; i++)//Get Business Base Price
+            {
+                if (BusinessCB.SelectedItem.ToString() == businessName[i])
+                {
+                    basePrice = businessContractPrice[i];
+                    break;
+                }
+            }
+
+            //Get Additional Discount
+            if (minYears >= contractYears[3])//11 or more years
+            {
+                additionalDisc = additionalDiscount[3];
+            }
+            else if (minYears >= contractYears[2])//7-10 years
+            {
+                additionalDisc = additionalDiscount[2];
+            }
+            else if (minYears >= contractYears[1])//3-6 years
+            {
+                additionalDisc = additionalDiscount[1];
+            }
+            else if (minYears >= contractYears[0])//0-2 years
+            {
+                additionalDisc = additionalDiscount[0];
+            }
+
+
+            //Calculate Total Contract Price
+            double totalPrice = basePrice - (basePrice * catererDisc) - additionalDisc;//Total Price Calculation
+            ContractPriceTotal.Text = totalPrice.ToString("C");//Display Total Price in Currency Format
+
+
+
+
+        }
+    }
+}
